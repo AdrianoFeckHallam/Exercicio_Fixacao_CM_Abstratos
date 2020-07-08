8c3122953adea1f93eba570d5cc070986fa945ed
AdrianoFeckHallam <feckadriano@gmail.com>
2020-07-08 02:21:15 -0300

    Implementando a classe Individual

--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class Individual {
+public class Individual extends TaxPayer {
+
+	private Double healthExpenditures;
+	
+	public Individual() {
+		super();
+	}	
+	
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+	public Double getHeathExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHeathExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	@Override
+	public Double tax() {
+		if (getAnualIncome() < 20000.0) {
+			return getAnualIncome() * 0.15 - healthExpenditures * 0.5;
+		}
+		else {
+			return getAnualIncome() * 0.25 - healthExpenditures * 0.5;
+		}
+	}
 
 }
